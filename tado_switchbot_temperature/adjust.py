05d6ccc522945b56dcfd0374f2b680047e382d97
Tim Pietz <tim@pietz.me>
2022-11-28 21:34:39 +0100

    Use the logging module instead of prints

--- tado_switchbot_temperature/adjust.py
+++ tado_switchbot_temperature/adjust.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List
 
 from switchbot_client import SwitchBotClient
@@ -5,21 +6,22 @@ from switchbot_client.devices import MeterPlusUs, MeterPlusJp
 
 from tado_switchbot_temperature.config import settings, SyncDevice
 
+logger = logging.getLogger(__name__)
+
 
 def adjust():
-    print(f'Tado username is {settings["tado.username"]} and password is {settings["tado.password"]}')
+    logger.info(f'Tado username is {settings["tado.username"]} and password is {settings["tado.password"]}')
 
     switchbot = SwitchBotClient(token=settings['switchbot.open_token'])
 
     sync_devices: List[SyncDevice] = settings['devices']
 
     for sync_device in sync_devices:
-        print(sync_device)
         meter = switchbot.device(sync_device['meter_id'])
         if not isinstance(meter, (MeterPlusUs, MeterPlusJp)):
             raise InvalidMeterError()
 
-        print(f'{meter.device_name} reports a temperature of {meter.temperature()} °C')
+        logger.info(f'{meter.device_name} reports a temperature of {meter.temperature()} °C')
 
 
 class InvalidMeterError(Exception):
