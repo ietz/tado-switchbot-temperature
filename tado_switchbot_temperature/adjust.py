2da4ec76d04e7a0fc00b11c314080745ef78f3b9
Tim Pietz <tim@pietz.me>
2022-11-28 21:22:21 +0100

    Print the temperature of all configured devices

--- tado_switchbot_temperature/adjust.py
+++ tado_switchbot_temperature/adjust.py
@@ -1,5 +1,26 @@
-from tado_switchbot_temperature.config import settings
+from typing import List
+
+from switchbot_client import SwitchBotClient
+from switchbot_client.devices import MeterPlusUs, MeterPlusJp
+
+from tado_switchbot_temperature.config import settings, SyncDevice
 
 
 def adjust():
-    print(f'Tado username is {settings["username"]} and password is {settings["password"]}')
+    print(f'Tado username is {settings["tado.username"]} and password is {settings["tado.password"]}')
+
+    switchbot = SwitchBotClient(token=settings['switchbot.open_token'])
+
+    sync_devices: List[SyncDevice] = settings['devices']
+
+    for sync_device in sync_devices:
+        print(sync_device)
+        meter = switchbot.device(sync_device['meter_id'])
+        if not isinstance(meter, (MeterPlusUs, MeterPlusJp)):
+            raise InvalidMeterError()
+
+        print(f'{meter.device_name} reports a temperature of {meter.temperature()} °C')
+
+
+class InvalidMeterError(Exception):
+    pass
--- tado_switchbot_temperature/config.py
+++ tado_switchbot_temperature/config.py
@@ -1,6 +1,13 @@
+from typing import TypedDict
+
 from dynaconf import Dynaconf
 
 settings = Dynaconf(
     envvar_prefix="TADO_SB",
     settings_files=['settings.toml', '.secrets.toml'],
 )
+
+
+class SyncDevice(TypedDict):
+    meter_id: str
+    valve_id: str
