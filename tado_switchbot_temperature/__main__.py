eb74f892d543f7320b2d0c4900b786138980cdc3
Tim Pietz <tim@pietz.me>
2022-11-30 21:47:38 +0100

    Add argparse with sync subcommand

--- tado_switchbot_temperature/__main__.py
+++ tado_switchbot_temperature/__main__.py
@@ -1,6 +1,15 @@
-from tado_switchbot_temperature.sync import sync
+import argparse
+
 from tado_switchbot_temperature.config import settings
 from tado_switchbot_temperature.logging import configure_logging
+from tado_switchbot_temperature.sync import sync
 
 configure_logging(log_file_name=settings.get('log_file', None))
-sync()
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(required=True)
+
+parser_sync = subparsers.add_parser('sync')
+parser_sync.set_defaults(func=sync)
+
+args = parser.parse_args()
+args.func()
